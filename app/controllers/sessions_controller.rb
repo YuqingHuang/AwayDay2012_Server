66f9418ce516ab0b418db901ef91cf643c21a6fa
Mark Ryall <mark@ryall.name>
2012-08-09 19:42:14 +0800

    changed the hard coded response to use symbols to eliminate hash rockets

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
   def grouped_by_date
     render text: [
-      { "agenda_date" => "2012-08-10",
-        "agenda_sessions" => [
-          { "session_description" => "the desc",
-            "settion_title" => "the title"},
-          { "session_description" => "the desc",
-            "settion_title" => "the title"}
+      { agenda_date: "2012-08-10",
+        agenda_sessions: [
+          { session_description: "the desc",
+            settion_title: "the title"},
+          { session_description: "the desc",
+            settion_title: "the title"}
         ]
       }
     ].to_json
