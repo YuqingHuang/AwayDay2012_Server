4707001d1cfd55e7d1e80ecfad64f61ad3d79328
Mark Ryall <mark@ryall.name>
2012-08-09 20:04:13 +0800

    simplified controller returning json

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def grouped_by_date
-    render text: [
+    render json: [
       { agenda_date: "2012-08-10",
         agenda_sessions: [
           { session_description: "the desc",
@@ -9,6 +9,6 @@ class SessionsController < ApplicationController
             settion_title: "the title"}
         ]
       }
-    ].to_json
+    ]
   end
 end
