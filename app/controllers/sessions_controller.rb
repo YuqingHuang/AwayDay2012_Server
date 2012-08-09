6d6d816cda440a2536ea41fe39ce8ccc30dbfb57
Mark Ryall <mark@ryall.name>
2012-08-09 19:29:56 +0800

    modified agenda json response structure

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
 class SessionsController < ApplicationController
   def grouped_by_date
     render text: [
-      {
-        'Friday' =>  []
-      },
-      {
-         'Saturday' => []
+      { "agenda_date" => "2012-08-10",
+        "agenda_sessions" => [
+          { "session_description" => "the desc",
+            "settion_title" => "the title"},
+          { "session_description" => "the desc",
+            "settion_title" => "the title"}
+        ]
       }
     ].to_json
   end
