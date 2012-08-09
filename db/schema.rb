544c971c3fb6f6aafa2dea2df67b2989dd0301ba
Mark Ryall <mark@ryall.name>
2012-08-09 19:38:01 +0800

    added the session model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20120809113306) do
+
+  create_table "sessions", :force => true do |t|
+    t.date     "date"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
 end
