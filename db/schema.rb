c7f960bd3c8082721acfa4f42ef50848a6a4ebc1
Mark Ryall <mark@ryall.name>
2012-08-14 18:47:38 +0800

    added migration for moments table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120813113732) do
+ActiveRecord::Schema.define(:version => 20120814104639) do
 
   create_table "sessions", :force => true do |t|
     t.date     "date"
