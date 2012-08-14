a582eb80f434a26dd5c752c2bd6105290a4bc7aa
Mark Ryall <mark@ryall.name>
2012-08-14 18:44:14 +0800

    created entry point for deleting a moment

--- app/controllers/moment_controller.rb
+++ app/controllers/moment_controller.rb
@@ -2,6 +2,17 @@ class MomentController < ApplicationController
   def create
     timestamp = params[:timestamp]
     device_id = params[:device_id]
+    logger.info "creating moment #{timestamp} for device_id #{device_id}"
+    render json: {
+      result: 'done',
+      timestamp: timestamp
+    }
+  end
+
+  def destroy
+    timestamp = params[:timestamp]
+    device_id = params[:device_id]
+    logger.info "destroying moment #{timestamp} for device_id #{device_id}"
     render json: {
       result: 'done',
       timestamp: timestamp
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ AwayDay2012Rails::Application.routes.draw do
   get 'sessions_grouped_by_date' => 'sessions#grouped_by_date'
 
   post 'moment' => 'moment#create'
+  delete 'moment' => 'moment#destroy'
 end
