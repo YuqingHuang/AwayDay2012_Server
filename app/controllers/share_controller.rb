a32dff7119c2b4566d4081981a0b84795a7161d2
NickZhang <looloopig@gmail.com>
2012-08-14 19:10:54 +0800

    Finished all the params for creating a share

--- app/controllers/share_controller.rb
+++ app/controllers/share_controller.rb
@@ -2,10 +2,14 @@ class ShareController < ApplicationController
   def create
     session_id = params[:session_id]
     timestamp = params[:timestamp]
+    device_id = params[:device_id]
+    username = params[:username]
+    share_text = params[:share_text]
+    share_image = params[:share_image]
     logger.info "creating share #{timestamp} for session_id #{session_id}"
     render json: {
       timestamp: timestamp,
-      seesionid: session_id
+      message: "Successful"
     }
   end
 end
