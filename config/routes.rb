61e5389476b7a2dd8bcbfe736e0c08c28bbce812
leizhang <leizhang@thoughtworks.com>
2012-10-09 17:47:43 +0800

    add messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 AwayDay2012Rails::Application.routes.draw do
+  resources :messages
+
   get 'sessions_grouped_by_date' => 'sessions#grouped_by_date'
 
   post 'moment' => 'moment#create'
@@ -6,4 +8,5 @@ AwayDay2012Rails::Application.routes.draw do
   post 'share' => 'share#create'
   get 'refresh_token' => 'token#refresh'
   get '/callback' => 'token#callback'
+  get 'notification' => 'messages#notify'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120815093252) do
+ActiveRecord::Schema.define(:version => 20120928092636) do
+
+  create_table "messages", :force => true do |t|
+    t.string   "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "moments", :force => true do |t|
     t.string   "device_id"
